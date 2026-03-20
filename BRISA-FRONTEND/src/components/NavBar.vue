030ae1ee5cfdb82dcff63f8b5011a25182ff50fd
Athisson Alberto <athissonlm17@gmail.com>
2026-03-19 21:35:26 -0300

    BRISA -> BRISA ONE

--- BRISA-FRONTEND/src/components/NavBar.vue
+++ BRISA-FRONTEND/src/components/NavBar.vue
@@ -12,7 +12,7 @@
         <div class="nav-brand">
           <router-link to="/dashboard" class="brand-link">
             <img src="https://inscricoesrestic.brisabr.com.br/_next/image?url=%2Flogo_no-txt.png&w=256&q=75" alt="logo" class="logo">
-            <span class="brand-name">BRISA</span>
+            <span class="brand-name">BRISA ONE</span>
           </router-link>
         </div>
       </div>
